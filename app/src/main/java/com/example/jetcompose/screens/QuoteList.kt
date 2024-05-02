b37f4ad4e9ee89e38f1f32b11f55d2e7ae626001
“Abhishekiroid” <dimpy@iroidsolutions.com>
2024-05-02 16:44:22 +0530

    intial commit

--- app/src/main/java/com/example/jetcompose/screens/QuoteList.kt
+++ app/src/main/java/com/example/jetcompose/screens/QuoteList.kt
@@ -3,7 +3,6 @@ package com.example.jetcompose.screens
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetcompose.data.Quote
 
 
