6e7a7bc41c770e5e9a24e94b8a0a8d70daf1bc1c
“Abhishekiroid” <dimpy@iroidsolutions.com>
2024-05-02 16:43:16 +0530

    intial commit

--- app/src/main/java/com/example/jetcompose/screens/ProduceState.kt
+++ app/src/main/java/com/example/jetcompose/screens/ProduceState.kt
@@ -1,14 +1,69 @@
 package com.example.jetcompose.screens
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcompose.R
 import kotlinx.coroutines.delay
 
-@Composable
+
+
 @Preview(showSystemUi = true)
+@Composable
+private fun animationImage(){
+    var isRotated by rememberSaveable { mutableStateOf(false) }
+    val rotate by animateFloatAsState(targetValue = if (isRotated) 360F else 0f,
+        animationSpec = tween(2000), label = ""
+    )
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.ic_fan),
+            contentDescription = "fan",
+            modifier = Modifier
+                .rotate(rotate)
+                .size(150.dp)
+        )
+
+        Button(
+            onClick = { isRotated = !isRotated },
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .width(200.dp)
+        ) {
+            Text(text = "Rotate Fan")
+        }
+    }
+}
+
+@Composable
+
 fun ProduceState() {
     // In produce state it used to state that it will produce two work
     // 1. state is being produce  2. it will update value but that is define by dev
@@ -22,4 +77,32 @@ fun ProduceState() {
     }
     Text(text = state.value.toString(),
         style = MaterialTheme.typography.bodyMedium)
+}
+
+
+//2. Animate*AsState
+//It's used for animating a single value. It can be Dp, Color, Float, Integer, Offset, Rect, Size.
+@Composable
+private fun AnimatableSample() {
+    var isAnimated by remember { mutableStateOf(false) }
+
+    val color = remember { Animatable(Color.DarkGray) }
+
+    // animate to green/red based on `button click`
+    LaunchedEffect(isAnimated) {
+        color.animateTo(if (isAnimated) Color.Green else Color.Red, animationSpec = tween(1000))
+    }
+
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.8f)
+            .background(color.value)
+    )
+    Button(
+        onClick = { isAnimated = !isAnimated },
+        modifier = Modifier.padding(top = 10.dp)
+    ) {
+        Text(text = "Animate Color")
+    }
 }
--- app/src/main/java/com/example/jetcompose/screens/QuoteList.kt
+++ app/src/main/java/com/example/jetcompose/screens/QuoteList.kt
@@ -3,7 +3,9 @@ package com.example.jetcompose.screens
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetcompose.data.Quote
+
 
 @Composable
 fun QuoteList(data: Array<Quote>,onClick:(quote: Quote)->Unit) {
